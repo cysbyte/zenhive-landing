2a0be331106d062e92c8bb237be70a1d71080b8f
cysbyte <chenyunshu2@gmail.com>
2024-10-17 23:47:07 +0800

    feat: add responsive for mobile page

--- src/components/home/Feature.tsx
+++ src/components/home/Feature.tsx
@@ -8,11 +8,11 @@ const Feature = () => {
   return (
     <Container>
         <div className='px-10 lg:px-20 py-20 gap-10 lg:gap-1'>
-            <h2 className='font-dmsans font-[700] text-[35px] lg:text-[42px] leading-[52px] tracking-[-0.4px] text-[#1E0E62]'>ZenHive Commercial Adoption</h2>
+            <h2 className='font-dmsans font-[700] text-[30px] lg:text-[42px] leading-[52px] tracking-[-0.4px] text-[#1E0E62]'>ZenHive Commercial Adoption</h2>
             <Features/>
-            <h2 className='mt-20 font-dmsans font-[700] text-[35px] lg:text-[42px] leading-[52px] tracking-[-0.4px] text-black'>ZenHive’s Key Features</h2>
+            <h2 className='mt-20 font-dmsans font-[700] text-[30px] lg:text-[42px] leading-[52px] tracking-[-0.4px] text-black'>ZenHive’s Key Features</h2>
             <KeyFeatures/>
-            <h2 className='mt-20 font-dmsans font-[500] text-[30px] lg:text-[36px] leading-[40px] tracking-[-0.4px] text-[#1E0E62]'>Join the DePIN Revolution: Select Your ZenHive Role Now!</h2>
+            <h2 className='mt-20 font-dmsans font-[500] text-[27px] lg:text-[36px] leading-[40px] tracking-[-0.4px] text-[#1E0E62]'>Join the DePIN Revolution: Select Your ZenHive Role Now!</h2>
             <JoinDepin/>
             <h2 className='mt-20  font-[500] text-[30px] lg:text-[36px] leading-[32px] tracking-[-0.4px] text-[#1E0E62]'>Join the Early Access List</h2>
             <p className='mt-10 font-medium  font-dmsans text-[#15143966] text-base'>Sign up now for exclusive updates and be among the first to embark on this exciting journey with us!</p>
--- src/components/home/GetLatest.tsx
+++ src/components/home/GetLatest.tsx
@@ -30,8 +30,8 @@ const GetLatest = () => {
   ]
   return (
     <Container>
-      <div className='px-10 md:px-20 py-20'>
-        <h2 className='font-[500] text-[36px] leading-[32px] tracking-[-0.4px] text-center'>Get the latest</h2>
+      <div className='px-10 md:px-20 py-10 md:py-20'>
+        <h2 className='font-[500] text-[30px] lg:text-[36px] leading-[32px] tracking-[-0.4px] text-center'>Get the latest</h2>
         <div className="flex flex-col md:flex-row gap-10 md:gap-6 items-start justify-between mt-20 ">
           {
             data.map((item) => (
