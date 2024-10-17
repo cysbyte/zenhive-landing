bfbf00adc0cd26eafa28cf0e7523f8f72685fde4
cysbyte <chenyunshu2@gmail.com>
2024-10-17 23:53:38 +0800

    feat: add responsive for mobile page

--- src/components/home/latest/Din.tsx
+++ src/components/home/latest/Din.tsx
@@ -1,11 +1,13 @@
 
 const Din = () => {
   return (
-    <div className='flex-1 w-full text-center text-white bg-[#1a1a1a] rounded-[3px]'>
-        <h6 className='text-[12px] uppercase px-4 py-1 bg-[#e26442] rounded-full mt-10 w-40 mx-auto'>New Networks</h6>
-        <h3 className='mx-auto text-lg w-[70%]'>DIN Summer Network Launches</h3>
-        <p className='uppercase mb-1 mt-10'>Infura</p>
-    </div>
+
+
+<div className='flex-1 w-full text-center text-white bg-[rgb(26,26,26)] rounded-[3px]'>
+<h6 className='text-[12px] uppercase px-4 py-1 bg-[#e26442] rounded-full mt-10 w-40 mx-auto'>New Networks</h6>
+<h3 className='text-lg mx-auto w-[60%]'>Best Practises for Infura API Key Management</h3>
+<p className='uppercase mb-1 mt-10'>Infura</p>
+</div>
   )
 }
 
