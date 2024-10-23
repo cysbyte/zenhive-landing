d2e0a5f294a05bb9aca1d59bc9681851d2bdaf97
cysbyte <chenyunshu2@gmail.com>
2024-10-23 15:39:03 +0800

    feat: add google auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import Home1 from './pages/Home1';
+import Signin from './pages/Signin';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
 
   return (
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID!}>
     <Router>
       <Routes>
         <Route path="/" element={<Home1 />} />
         <Route path="/home1" element={<Home />} />
+        <Route path='/signin' element={<Signin/>}/>
       </Routes>
     </Router>
+    </GoogleOAuthProvider>
   )
 }
 
